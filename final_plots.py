73964cde6f9a91762eaa533f14af11efcaead16c
victoriatascau <vickytascau@gmail.com>
2022-10-24 18:32:53 +0200

    Create final results for plotting and implement type1 plot

---
+++ final_plots.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from src.suggested_plots import *
+
+
+results_mean = pd.DataFrame(columns = ["mean_of_means", "mean_of_biases", "mean_of_miss_percent", "mean_of_std", "mean_of_accuracy_a", "mean_of_accuracy_b"])
+results_std = pd.DataFrame(columns = ["std_of_means", "std_of_biases", "std_of_miss_percent", "std_of_std", "std_of_accuracy_a", "std_of_accuracy_b"])
+
+dic_m = {}
+dic_s = {}
+
+for i_mcar in [0, 10, 20]:
+    for i_mar in [0, 10, 20]:
+        for i_mnar in [0, 10, 20]:
+            if i_mcar == 0 and i_mar == 0 and i_mnar == 0:
+                continue
+            csv_name = "./csvs/MCAR:" + str(i_mcar) + "_" + "MAR:" + str(i_mar) + "_" + "MNAR:" + str(i_mnar) + ".csv"
+            df = pd.read_csv(csv_name)
+            # print(df.head(3))
+            n = df.shape[1]
+            # print(n, df.dtypes)
+            m = [0] * (n - 1)
+            s = [0] * (n - 1)
+            idx = 0
+            for i in range(0, n):
+                if i == 2:
+                    continue
+                m[idx] = df.iloc[:, i].mean()
+                s[idx] = df.iloc[:, i].std()
+                idx += 1
+            # print(m)
+            # print(s)
+
+            series_m = pd.Series({"mean_of_means": m[0], "mean_of_biases": m[1], "mean_of_miss_percent": m[2], "mean_of_std": m[3], "mean_of_accuracy_a": m[4], "mean_of_accuracy_b": m[5]})
+            results_mean = pd.concat([results_mean, series_m.to_frame().T], ignore_index=True)
+            series_s = pd.Series({"std_of_means": s[0], "std_of_biases": s[1], "std_of_miss_percent": s[2], "std_of_std": s[3], "std_of_accuracy_a": s[4], "std_of_accuracy_b": s[5]})
+            results_std = pd.concat([results_std, series_s.to_frame().T], ignore_index=True)
+
+            tup = (i_mcar, i_mar, i_mnar)
+            dic_m[tup] = m
+            dic_s[tup] = s
+
+# print(results_mean)
+# print(dic_m)
+
+txt_name = "./coverage_rates.txt"
+df = pd.read_csv(txt_name, delim_whitespace=True)
+df.drop(df.columns[[0]], axis = 1, inplace=True)
+# print(df)
+
+# Final plots
+
+# print(results_mean.iloc[:, 0])
+
+barplot_type1(dic_m, dic_s, x_axis_miss_type="MNAR", legend_miss_type="MCAR", title_miss_type="MAR", percentage=20, column=0)
+
+
---
+++ src/suggested_plots.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def determine_tuple(tup_perm: tuple, t: int, l: int, x: int):
+    tup = ()
+    match tup_perm:
+        case (0, 1, 2):
+            tup = (t, l, x)
+        case (0, 2, 1):
+            tup = (t, x, l)
+        case (1, 0, 2):
+            tup = (l, t, x)
+        case (1, 2, 0):
+            tup = (l, x, t)
+        case (2, 0, 1):
+            tup = (x, t, l)
+        case (2, 1, 0):
+            tup = (x, l, t)
+    return tup
+
+
+def determine_order(t: str, l: str, x: str):
+    tup = ()
+    match (t, l, x):
+        case ("MCAR", "MAR", "MNAR"):
+            tup = (0, 1, 2)
+        case ("MCAR", "MNAR", "MAR"):
+            tup = (0, 2, 1)
+        case ("MAR", "MCAR", "MNAR"):
+            tup = (1, 0, 2)
+        case ("MAR", "MNAR", "MCAR"):
+            tup = (2, 0, 1)
+        case ("MNAR", "MCAR", "MAR"):
+            tup = (1, 2, 0)
+        case ("MNAR", "MAR", "MCAR"):
+            tup = (2, 1, 0)
+    return tup
+
+
+def barplot_type1(dic_m, dic_s, x_axis_miss_type: str, legend_miss_type: str, title_miss_type: str, percentage: int, column: int):
+    tup_perm = determine_order(title_miss_type, legend_miss_type, x_axis_miss_type)
+   
+    light = [] 
+    medium = [] 
+    dark = [] 
+    # For data for different plots, just change the miss_p and range list.
+    for t in [percentage]:
+        for idx_2, l in enumerate([0, 10, 20]):
+            aux = [0] * 3
+            for idx_1, x in enumerate([0, 10, 20]):
+                if t == 0 and l == 0 and x == 0:
+                    continue
+                tup = determine_tuple(tup_perm, t, l, x)
+                # print(tup)
+                aux[idx_1] = dic_m[tup][column]
+            
+            if idx_2 == 0:
+                light = aux
+            elif idx_2 == 1:
+                medium = aux
+            else:
+                dark = aux
+
+    # print("l: ", np.around(light, decimals=6))
+    # print("m: ", np.around(medium, decimals=6))
+    # print("d: ", np.around(dark, decimals=6))
+
+    # x axis label
+    x_label = x_axis_miss_type
+    # y axis label
+    y_label = "mean"
+    # plot title
+    plot_title = title_miss_type + " " + str(percentage)
+    # legend title
+    legend_title = legend_miss_type
+
+    N = 3
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.27       # the width of the bars
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    rects_light = ax.bar(ind, light, width, color='skyblue')
+    rects_medium = ax.bar(ind+width, medium, width, color='dodgerblue')
+    rects_dark = ax.bar(ind+width*2, dark, width, color='navy')
+
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_xticks(ind+width)
+    ax.set_xticklabels( ('0%', '10%', '20%') )
+    ax.legend( (rects_light[0], rects_medium[0], rects_dark[0]), ('0%', '10%', '20%'), title=legend_title)
+
+    def autolabel(rects):
+        for rect in rects:
+            h = rect.get_height()
+            ax.text(rect.get_x()+rect.get_width()/2., 1.005*h, '%.4f'%float(h),
+                    ha='center', va='bottom')
+
+    autolabel(rects_light)
+    autolabel(rects_medium)
+    autolabel(rects_dark)
+
+    plt.ylim([0.09, 0.1])
+    plt.title(plot_title)
+    plt.show()
